c3f82340ed8d4b10d1216d2d2c92c4de83d3f6ac
Adithya Yerrabothu <yerrabothuadithya183@gmail.com>
2025-08-17 00:11:45 +0530

    Fix timezone parsing

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,13 @@
+const toUTCDate = (d: string | number | Date) =>
+  typeof d === 'string' && !/[zZ]|[+-]\d{2}:\d{2}$/.test(d)
+    ? new Date(`${d}Z`) // assume server sent UTC
+    : new Date(d);
+
 export const formatIST = (
   date: string | number | Date,
-  options?: Intl.DateTimeFormatOptions,
+  options?: Intl.DateTimeFormatOptions
 ) =>
-  new Date(date).toLocaleString('en-IN', { timeZone: 'Asia/Kolkata', ...options });
+  toUTCDate(date).toLocaleString('en-IN', {
+    timeZone: 'Asia/Kolkata',
+    ...options,
+  });
