67ddafc8f5f15e2333cc74d2e2367a4c6ea98a2d
Adithya Yerrabothu <yerrabothuadithya183@gmail.com>
2025-08-19 00:02:59 +0530

    feat: implement B2 file routes

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ const studentRoutes = require('./routes/students');
 const notificationRoutes = require('./routes/notifications');
 const conversationRoutes = require('./routes/conversations');
 const uploadRoutes = require('./routes/uploads');
+const fileRoutes = require('./routes/files');
 const professorRoutes = require('./routes/professors');
 const publicRoutes = require('./routes/public');
 
@@ -69,6 +70,7 @@ app.use('/api/students', studentRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/conversations', conversationRoutes);
 app.use('/api/uploads', uploadRoutes);
+app.use('/api/files', fileRoutes);
 app.use('/api/professors', professorRoutes);
 app.use('/api/resumes', require('./routes/resumes'));
 app.use('/api/alumni', require('./routes/alumni'));
