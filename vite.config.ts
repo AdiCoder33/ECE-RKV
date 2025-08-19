d1725a94cfb6335c5a24c722c82a02cb2d795800
Adithya Yerrabothu <yerrabothuadithya183@gmail.com>
2025-08-19 12:18:51 +0530

    fix: update PWA icon paths

--- vite.config.ts
+++ vite.config.ts
@@ -34,8 +34,18 @@ export default defineConfig(({ mode }) => {
           display: "standalone",
           start_url: "/",
           icons: [
-            { src: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
-            { src: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png" },
+            {
+              src: "/icons/manifest-icon-192.maskable.png",
+              sizes: "192x192",
+              type: "image/png",
+              purpose: "any maskable",
+            },
+            {
+              src: "/icons/manifest-icon-512.maskable.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any maskable",
+            },
           ],
         },
       }),
