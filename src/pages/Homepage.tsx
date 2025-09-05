b00bfece4756d1e2fc2d2439f1b0dbffd179d251
YERRABOTHU ADITHYA <yerrabothuadithya183@gmail.com>
2025-09-05 23:21:26 +0530

    Update Homepage.tsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -50,7 +50,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 // NEW: Import the modal components and define the faculty type
-import FacultyDetailsModal from "../components/FacultyDetailsModal";
+import FacultyDetailsModal from "../components/users/FacultyDetailsModal";
 
 interface Announcement {
   author: string;
